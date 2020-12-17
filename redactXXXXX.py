d1cb47c5e6246b56c790c2cfc5307c44d415f377
Dennis Verzijl <dnsverzijl@gmail.com>
2020-12-17 20:26:26 +0100

    works

--- redactXXXXX.py
+++ redactXXXXX.py
@@ -3,11 +3,11 @@ import re
 XXXX redacted
 """
 def redact(sent, word, case=False, whole=False, overkill=False):
-    flags = re.I if not case else 0
+    caseflag =  0 if case else re.I
 
     def replacer(mo):
         l = mo.end(0) - mo.start(0)
-        return 'X'*l
+        return 'X' * l
 
     word0 = word
 
@@ -16,14 +16,13 @@ def redact(sent, word, case=False, whole=False, overkill=False):
     if overkill:
         word0 = r'\b' + r'\w*' + word + r'\w*' + r'\b'
         
-    #split so can rejoin later
-    return re.sub(word0, replacer, sent, flags=re.I)
+    return re.sub(word0, replacer, sent, flags=caseflag)
 
 test = "Toms bottom tomato is in his stomach while playing the 'Tom-tom' brand tom-toms. That's so tom."
 
 print(redact(test, 'Tom', case=True, whole=True))
-print(redact(test, 'Tom', case=True, whole=True))
-print(redact(test, 'Tom', case=True, whole=False))
-print(redact(test, 'Tom', case=True, whole=False, overkill=True))
+print(redact(test, 'Tom', whole=True))
+print(redact(test, 'Tom', whole=False))
+print(redact(test, 'Tom', whole=False, overkill=True))
 
 
