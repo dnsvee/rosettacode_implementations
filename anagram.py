54af25d595b29ee9c60d39d5ba075d4c8cae39c3
Dennis Verzijl <dnsverzijl@gmail.com>
2020-12-20 15:38:26 +0100

    donish

--- anagram.py
+++ anagram.py
@@ -1,18 +1,28 @@
 """
-anagram rosettacode
+Rosettacode task Anagrams
 """
 from itertools import groupby
 words = []
-with open('dict.txt') as d:
+with open('unixdict.txt') as d:
     words = d.readlines()
+
+# strip blanks
 words = map    (lambda x: x.strip()              , words)
-words = map    (lambda x: ("".join(sorted(x)), x), words)
-words = sorted (words,    key=lambda x: x[0])
-words = groupby(words,    key=lambda x : x[0])
-words = map    (lambda x: (x[0], list(x[1])), words)
-words = map    (lambda x: (x[0], list(map(lambda y: y[1], x[1]))), words)
-words = sorted (words,    key=lambda x: len(x[1]))
-words = list(words)
-maxlen= len(words[-1][1])
-words = filter(lambda x: len(x[1]) == maxlen, words)
-print(list(map(lambda x: x[1], words)))
+
+def sortletters(w):
+    return "".join(sorted(w))
+
+grouped = {}
+for w in words:
+    c = sortletters(w)
+    s = grouped.get(c, set())
+    s.add(w)
+    grouped[c] = s
+
+m = 0
+for k,v in grouped.items():
+    m = m if len(v) < m else len(v)
+
+for k,v in grouped.items():
+    if len(v) == m:
+        print(v)
