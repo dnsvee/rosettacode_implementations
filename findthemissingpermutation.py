3a6846ece0de5991e63432f5838e694f2c61cb50
Dennis Verzijl <dnsverzijl@gmail.com>
2020-12-20 15:57:22 +0100

    donish

--- findthemissingpermutation.py
+++ findthemissingpermutation.py
@@ -1,15 +1,11 @@
 import collections
 
 perms = "abcd cabd acdb dacb bcda acbd adcb cdab dabc bcad cadb cdba cbad abdc adbc bdca dcba bacd badc bdac cbda dbca dcab".split(" ")
-print(perms)
-cols = []
-for c in range(0,4):
-    row = []
-    for p in perms:
-        row.append(p[c])
-    cols.append(row)
+cols = [[p[i] for p in perms] for i in range(0,4)]
 
 cols = list(map(lambda x: collections.Counter(x), cols))
+
+# should be 6 of the same letter in every column. if only 5 present then it is the missing permutation
 sol = ""
 for c in cols:
     for k,v in c.items():
