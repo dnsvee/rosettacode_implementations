641c5eebddfccefd32cd1fe607ff0c20b084821b
Dennis Verzijl <dnsverzijl@gmail.com>
2020-12-30 16:40:41 +0100

    done

--- abcwords.py
+++ abcwords.py
@@ -6,14 +6,14 @@ def readdict():
     with open('dict.txt', 'r') as f:
         return f.read().split('\n')
 
-c = 0
+i = 1
 for w in readdict():
     try:
         a = w.index('a')
         b = w.index('b')
         c = w.index('c')
         if c > b > a:
-            print(c, w)
-            c += 1
+            print(i, w)
+            i += 1
     except:
         pass
