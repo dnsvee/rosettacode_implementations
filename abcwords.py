b840e8890eec3141da9529f14f8c662485790a84
Dennis Verzijl <dnsverzijl@gmail.com>
2020-12-30 16:39:32 +0100

    done

--- abcwords.py
+++ abcwords.py
@@ -3,15 +3,17 @@ ABC Words
 """
 
 def readdict():
-    with open('unixdict.txt', 'r') as f:
+    with open('dict.txt', 'r') as f:
         return f.read().split('\n')
 
+c = 0
 for w in readdict():
     try:
         a = w.index('a')
         b = w.index('b')
         c = w.index('c')
         if c > b > a:
-            print(w)
+            print(c, w)
+            c += 1
     except:
         pass
