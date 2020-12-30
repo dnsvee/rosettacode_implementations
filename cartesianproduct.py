21e2d9898c61309367c06e1b9ea52dcb658d59b2
Dennis Verzijl <dnsverzijl@gmail.com>
2020-12-30 16:38:01 +0100

    done

--- cartesianproduct.py
+++ cartesianproduct.py
@@ -1,9 +1,13 @@
+"""
+Cartesian product of two or more lists
+"""
+
 def cartprod(a,b):
     return [(a0,b0) for a0 in a for b0 in b]
 
-print(cartprod([1], [2]))
-print(cartprod([1,2,3] ,[3,4,5]))
-print(cartprod([], [1]))
-print(cartprod([2], []))
+print(cartprod([1,2], [3,4]))
+print(cartprod([3,4] ,[1,2]))
+print(cartprod([1,2], []))
+print(cartprod([], [1,2]))
 print(cartprod([], []))
 
