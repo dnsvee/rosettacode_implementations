8abcdeb731c00139a0ce45aede64ebfaec64d862
Dennis Verzijl <dnsverzijl@gmail.com>
2020-12-30 18:22:59 +0100

    done

--- jewelsandstones.py
+++ jewelsandstones.py
@@ -1,13 +1,8 @@
 """
-    rosettacode: jewels and stones
+Jewels and stones
 """
 def howmanyjewels(s, j):
-    cnt = 0
-    for s0 in s:
-        if s0 in j:
-            cnt += 1
-
-    return cnt
+    return len([s0 for s0 in s if s0 in j])
 
 print(howmanyjewels('aAAbbbb', 'aA'))
 
