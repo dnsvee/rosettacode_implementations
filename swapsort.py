3f4891914737cd70fdb7ddb52c388667083cb74f
Dennis Verzijl <dnsverzijl@gmail.com>
2021-01-06 22:26:03 +0100

    problematic

--- swapsort.py
+++ swapsort.py
@@ -49,7 +49,7 @@ def qsort(l):
         return l
 
     b, m, a = [], [], []
-    p = l[random.randrange(0, len(l))]
+    p = l[len(l) // 2]
     while len(l):
         i = l.pop()
         if i < p: b.append(i)
@@ -57,10 +57,51 @@ def qsort(l):
         if i == p: m.append(i)
     return b + m + a
 
+def merge(a, b):
+    i = j = 0
+    r = []
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
+            r.append(a[i])
+            i += 1
+        else:
+            r.append(b[j])
+            j += 1
+    if i < len(a):
+        r.extend(a[i:])
+    if j < len(b):
+        r.extend(b[j:])
+
+    return r
+
+def mergesort(l):
+    trail = []
+    if len(l) < 2:
+        return l
+    trail.extend([1,1])
+    upto = 2
+    while True:
+        if len(trail)>1:
+            b = trail[-1]
+            a = trail[-2]
+            if a == b:
+                x = merge(l[upto-b-a:upto-b], l[upto-b:upto])
+                l[upto-a-b:upto] = x
+                trail[-2] += trail[-1]
+                trail.pop()
+                continue
+        if upto < len(l):
+            trail.append(1)
+            upto += 1
+        if upto == len(l) and len(trail) == 1:
+            break
+    return l
+
 b = clockit(swapsort, "swapsort", bunch[:])
 b = clockit(swapsort, "swapsort sorted", b)
 b = clockit(selsort, "selection sort", bunch[:])
 b = clockit(qsort, "quicksort", bunch[:])
+b = clockit(mergesort, "mergesort", bunch[:])
 
 
 
