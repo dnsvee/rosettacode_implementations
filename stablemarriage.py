0b5b4363a8efffaaeb963e301ac17c82c4601336
Dennis Verzijl <dnsverzijl@gmail.com>
2021-02-22 11:39:27 +0100

    better now

--- stablemarriage.py
+++ stablemarriage.py
@@ -1,16 +1,15 @@
 """
-First attempt at stable-marriage problem
+Implementing the stable marriage problem:
 """
-
 
 class Marriage:
     def __init__(self):
-        self.Men          = {}
-        self.Women        = {}
-        self.EngagedTo    = {}
-        self.total        = 0
+        self.Men          = {} # the man
+        self.Women        = {} # the woman
+        self.EngagedTo    = {} # dict of who's engaged to whom
 
-        ex = """
+        # least preferred to most preferred
+        theproblem = """
 abe: abi, eve, cath, ivy, jan, dee, fay, bea, hope, gay
 bob: cath, hope, abi, dee, eve, fay, bea, jan, ivy, gay
 col: hope, eve, abi, dee, bea, fay, ivy, gay, cath, jan
@@ -33,73 +32,56 @@ ivy: ian, col, hal, gav, fred, bob, abe, ed, jon, dan
 jan: ed, hal, gav, abe, bob, jon, col, ian, fred, dan
 """[1:-1].split('\n')
 
-        list1 = []
-        for l in ex:
-            prs, prefs = l.split(': ', 1)
+        data = []
+        for l in theproblem:
+            person, prefs = l.split(': ', 1)
             prefs = list(reversed(prefs.split(', ')))
-            list1.append((prs,prefs))
-
-        aman   = self.aman
-        awoman = self.awoman
-
-        for i in range(0,10):
-            aman(*list1[i])
-
-        for i in range(10,20):
-            awoman(*list1[i])
-
-        print(self.Men)
+            data += [(person, prefs)]
 
         Men   = self.Men
         Women = self.Women
 
-        while self.total < len(self.Men):
-            for m in self.Men:
-                if m in self.EngagedTo: 
-                    continue
-                self.proposed(self.propose(m), m)
+        for name, prefs in data[0:10]:
+            Men[name]   = prefs
 
-    # easy setters
-    def aman(self, n, prefs):
-        # prefs is list
-        self.Men[n]   = [ None, prefs ]
-
-    def awoman(self, n, prefs):
-        # prefs is dict
-        self.Women[n] = [ None, {k:i for i, k in enumerate(prefs)} ]
-
-    # return woman the man most prefers left over from those he has not proposed to so far
-    def propose(self, M):
-        return self.Men[M][1].pop()
-
-    # engages men and woman if woman prefers the men over whom she is engaged so she may rethink her engagement
-    def proposed(self, W, M):
-        woman = self.Women[W]
-        man   = self.Men[M]
+        for name, prefs in data[10:20]:
+            Women[name] = prefs
 
         EngagedTo = self.EngagedTo
-        woman_engagedto = EngagedTo.get(W, None)
-        if not woman_engagedto:
-            EngagedTo[W] = M
-            EngagedTo[M] = W
-            self.total += 1
-            print('getting engaged: ', M, W)
-            return
 
-        p1, p2 = woman[1][M], woman[1][woman_engagedto]
-        if p1 > p2:
-            print('breaking engagament: ', W, woman_engagedto)
-            print('getting engaged: ', M, W)
-            EngagedTo[W] = M
-            EngagedTo[M] = W
-            del EngagedTo[woman_engagedto]
+        while len(EngagedTo) < 20:
+            for man in self.Men:
+                # guy is already engaged
+                if man in self.EngagedTo: 
+                    continue
 
-    # uses chainmap for nexttime
+                # propose to the woman he most prefers over remaining
+                woman = Men[man].pop()
+
+                woman_engagedto = EngagedTo.get(woman, None)
+
+                # neither man or woman are engaged so engage them
+                if not woman_engagedto:
+                    EngagedTo[woman] = man
+                    EngagedTo[man]   = woman
+
+                    print(woman, ' getting engaged to ', man)
+                    continue
+
+                wprefs = Women[woman]
+                # see which man the woman prefers
+                if wprefs.index(man) > wprefs.index(woman_engagedto):
+                    print(woman, ' breaking engagement to ', woman_engagedto)
+                    print(woman, ' getting engaged to ', man)
+                    EngagedTo[woman] = man
+                    EngagedTo[man]   = woman
+                    del EngagedTo[woman_engagedto]
+
     def display(self):
+        print('all engagements')
         for k, v in self.EngagedTo.items():
                 print(k, ' engaged to ', v)
 
-Mars = Marriage()
-Mars.display()
+Marriage().display()
 
 
